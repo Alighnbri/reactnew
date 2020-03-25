9fa83b70ab3a5e697f02b3ec8353c492c5ba303c
Alighnbri <60141556+Alighnbri@users.noreply.github.com>
2020-03-25 15:24:04 +0430

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import $ from 'jquery';
+
 
 export class App extends Component {
     render() {
         return (
             <div>
-                <h1>welcome, React</h1>
-                <p>jsx performance</p>
-                <div className="servic">
-                    <header className="demo">
-                        <img src={require('../images/React.png')} alt="logo" className="logo-class" />
-                        <p>
-                            Edit <code>src/App.js</code> and save to reload.
+                <div class="row">
+                    <div class="col-xs-1-12">
+
+                        <h2>use of jquery</h2>
+                        <div className="servic">
+                            <button type="button" className="btn btn-warning">Warning</button>
+                        </div>
+                        <br/>
+                        <div className="social">
+                            <i className="fas fa-user-astronaut fa-lg"></i>
+                        </div>
+                        <h1>welcome, React</h1>
+                        <p>jsx performance</p>
+                        <div className="servic">
+                            <header className="demo">
+                                <img src={require('../images/React.png')} alt="logo" className="logo-class" />
+                                <p>
+                                    Edit <code>src/App.js</code> and save to reload.
                         </p>
-                        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                                <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 
-                            Learn React
+                                    Learn React
                          </a>
-                    </header>
+                            </header>
 
-                </div>
-                <div className="social_container">
-                    <a className="social_media" href="https://www.instagram.com/css__26" rel="svg logo" target="_blank"> <i className="fab fa-instagram"></i></a>
-                    <a className='social_media' target='_blank' href="https://github.com/alighnbri"> <i className='fab fa-github'></i></a>
-                    <a className='social_media' target='_blank' href="#"> <i className='fab fa-twitter'></i></a>
-                    <a className='social_media' target='_blank' href="#"> <i className='fab fa-youtube'></i></a>
-                    <a className='social_media' target='_blank' href="#"> <i className='fas fa-truck-monster fa-spin'></i></a>
-                    <a className='social_media' target='_blank' href="#"> <i className='fas fa-skull fa-lg'></i></a>
+                        </div>
+                        <div className="social_container">
+                            <a className="social_media" href="https://www.instagram.com/css__26" rel="svg logo" target="_blank"> <i className="fab fa-instagram"></i></a>
+                            <a className='social_media' target='_blank' href="https://github.com/alighnbri"> <i className='fab fa-github'></i></a>
+                            <a className='social_media' target='_blank' href="#"> <i className='fab fa-twitter'></i></a>
+                            <a className='social_media' target='_blank' href="#"> <i className='fab fa-youtube'></i></a>
+                            <a className='social_media' target='_blank' href="#"> <i className='fas fa-truck-monster fa-spin'></i></a>
+                            <a className='social_media' target='_blank' href="#"> <i className='fas fa-skull fa-lg'></i></a>
+
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
         )
     }
+    componentDidMount() {
+        $('button').click(function () {
+            $('h2').toggleClass('sinc');
+        });
+    }
+
 }
 
 export default App
+
 
